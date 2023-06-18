51c219ad119e0cdd2e8bed5b343dec3de3767dfb
AKA-Saad <saad@ztabs.co>
2023-06-18 22:24:23 +0500

    ranked successfully

--- app/ranks.php
+++ app/ranks.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Collection;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$scores = collect([
-    ['score' => 76, 'team' => 'A'],
+$scores = collect ([
+    ['score' => 91, 'team' => 'A'],
     ['score' => 62, 'team' => 'B'],
     ['score' => 82, 'team' => 'C'],
     ['score' => 86, 'team' => 'D'],
@@ -14,17 +14,19 @@ $scores = collect([
     ['score' => 67, 'team' => 'G'],
     ['score' => 82, 'team' => 'H'],
 ]);
+
 $ranks = $scores->sortByDesc('score')
     ->groupBy('score')
-    ->map(function ($group) {
-        return collect($group)->pluck('team')->values();
-    })
-    ->values()
-    ->map(function ($teams, $rank) {
-        return $teams->map(function ($team) use ($rank) {
-            return ['team' => $team, 'rank' => $rank + 1];
+    ->map(function ($group, $score) {
+        static $currentRank = 0;
+        $teamsCount = $group->count();
+        $rank = ++$currentRank;
+        $currentRank += $teamsCount - 1;
+        return $group->map(function ($team) use ($rank) {
+            return ['team' => $team['team'], 'rank' => $rank];
         });
     })
     ->collapse();
 
 print_r($ranks);
+
